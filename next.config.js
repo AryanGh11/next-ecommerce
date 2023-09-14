a7e97ae21ffd00c4415531c00379a4afbc1ffc6e
Aryan Gholami <mahdi.gholami875@gmail.com>
2023-09-14 21:14:44 +0330

    added manifest

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    sw: 'service-worker.js', // نام فایل Service Worker
+  },
+  // دیگر تنظیمات Next.js
+})
+
 const nextConfig = {
   experimental: {
     appDir: true,
